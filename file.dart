a571669b97930025a200cb20ea42323b407af1c7
wesley001 <wesleykinsman66@gmail.com>
2022-12-05 18:44:00 -0800

    repo

--- file.dart
+++ file.dart
@@ -1,3 +1,49 @@
-void main(List<String> args) {
-  print("we are in remote")
+// class Animal {
+//   String? name;
+//   int? numberofLegs;
+//   int? lifeSpan;
+
+//   void display() {
+//     print("Animal name: $name");
+//     print("Number of Legs: $numberofLegs.");
+//     print("Life Span: $lifeSpan.");
+//   }
+
+//   void animalNoise(String name) {
+//     this.name = name;
+
+//     switch (name) {
+//       case "Dog":
+//         print("cardinal Bark");
+//         break;
+//       default:
+//     }
+//   }
+// }
+
+// void main() {
+//   Animal dog = Animal();
+//   Animal cat = Animal();
+
+//   Animal.display(dog);
+// }
+
+// class Area {
+//   double? length;
+//   double? breadth;
+
+//   double (){
+
+//   }
+// }
+import 'dart:html';
+
+void main() {
+  Area Rectangle = Area();
+  Rectangle.length = 20;
+  Rectangle.calcuateArea();
+}
+
+class intrest{
+  
 }
